5fcfed35339ea82c067a2dc28fa402d8a86102cb
Eduardo Paludo <eduardo.paludo8@gmail.com>
2023-04-27 13:57:07 -0300

    cores e main criados

---
+++ Main.java
@@ -0,0 +1,9 @@
+// Eduardo Stefanel Paludo - GRR20210581
+// Lucas Neia Torres - GRR20210570
+// Natael Pontarolo Gomes - GRR2021
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
